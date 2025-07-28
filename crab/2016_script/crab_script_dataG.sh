8181a4431ce8b3be39679f14c6e539d43a596851
menglu21 <lumeng123@pku.edu.cn>
2025-07-28 17:52:30 +0200

    update

--- crab/2016_script/crab_script_dataG.sh
+++ crab/2016_script/crab_script_dataG.sh
@@ -13,14 +13,6 @@ echo "CMSSW BASE, python path, pwd"
 echo $CMSSW_BASE 
 echo $PYTHON_PATH
 echo $PWD 
-rm -rf $CMSSW_BASE/lib/
-rm -rf $CMSSW_BASE/src/
-rm -rf $CMSSW_BASE/module/
-rm -rf $CMSSW_BASE/python/
-mv lib $CMSSW_BASE/lib
-mv src $CMSSW_BASE/src
-mv modules $CMSSW_BASE/modules
-mv python $CMSSW_BASE/python
 
 echo Found Proxy in: $X509_USER_PROXY
 python crab_script.py -d --year "2016g"
